9780f910ade10de5933a3afbb9914b09108ec6d3
Fábio Mendes <fabiobends@gmail.com>
2021-06-22 13:17:37 -0300

    Add lessons 1 and 2

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+// import { useContext } from "react";
+// import { AuthContext } from "../contexts/AuthContext";
+
+export function NewHome() {
+  // const { user } = useContext(AuthContext);
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Perguntas e respostas" />
+        <strong>Crie salas de Q&A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real.</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          {/* <h1>{user?.name}</h1> */}
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>{" "}
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
